cf1dbb9ebf5abb5878eec574fea78daad17bb94b
Stepan <77981177+Maskvizy@users.noreply.github.com>
2024-05-16 14:58:34 +0300

    fix: moved topic-name property in application.properties

--- src/main/java/ru/stepagin/blps/service/KafkaProducerService.java
+++ src/main/java/ru/stepagin/blps/service/KafkaProducerService.java
@@ -1,6 +1,7 @@
 package ru.stepagin.blps.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import ru.stepagin.blps.dto.IssueDto;
@@ -9,9 +10,13 @@ import ru.stepagin.blps.dto.IssueDto;
 @RequiredArgsConstructor
 public class KafkaProducerService {
     private final KafkaTemplate<Long, IssueDto> kafkaTemplate;
+    @Value(value = "${app.kafka.topic-names.issue}")
+    private String issueTopicName;
+    @Value(value = "${app.kafka.topic-names.user}")
+    private String userTopicName;
 
     public void sendIssue(IssueDto issueDto) {
-        kafkaTemplate.send("issues", issueDto.getId(), issueDto);
+        kafkaTemplate.send(issueTopicName, issueDto.getId(), issueDto);
         kafkaTemplate.flush();
     }
 }
