30a2dd3e8f24066b7bf80109973b13d70722f3fa
Stepan <77981177+Maskvizy@users.noreply.github.com>
2024-05-19 19:54:12 +0300

    removed Security: unnecessary fields, methods and classes

--- subscription-service/src/main/java/com/annyarusova/subscriptionservice/service/AuthService.java
+++ subscription-service/src/main/java/com/annyarusova/subscriptionservice/service/AuthService.java
@@ -1,77 +1,18 @@
 package com.annyarusova.subscriptionservice.service;
 
-import com.annyarusova.subscriptionservice.config.SecurityConfiguration;
 import com.annyarusova.subscriptionservice.entity.UserEntity;
 import com.annyarusova.subscriptionservice.security.JwtAuthentication;
-import com.annyarusova.subscriptionservice.security.JwtProvider;
-import com.annyarusova.subscriptionservice.security.JwtRequest;
-import com.annyarusova.subscriptionservice.security.JwtResponse;
-import io.jsonwebtoken.Claims;
 import jakarta.security.auth.message.AuthException;
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
 
     private final UserService userService;
-    private final Map<String, String> refreshStorage = new HashMap<>();
-    private final JwtProvider jwtProvider;
-
-    @SneakyThrows
-    public JwtResponse login(@NonNull JwtRequest authRequest) {
-        final UserEntity user = userService.getByLogin(authRequest.getLogin())
-                .orElseThrow(() -> new AuthException("Пользователь не найден"));
-        if (SecurityConfiguration.passwordEncoder().matches(authRequest.getPassword(), user.getPassword())) {
-            final String accessToken = jwtProvider.generateAccessToken(user);
-            final String refreshToken = jwtProvider.generateRefreshToken(user);
-            refreshStorage.put(user.getLogin(), refreshToken);
-            return new JwtResponse(accessToken, refreshToken);
-        } else {
-            throw new AuthException("Неправильный пароль");
-        }
-    }
-
-    @SneakyThrows
-    public JwtResponse getAccessToken(@NonNull String refreshToken) {
-        if (jwtProvider.validateRefreshToken(refreshToken)) {
-            final Claims claims = jwtProvider.getRefreshClaims(refreshToken);
-            final String login = claims.getSubject();
-            final String saveRefreshToken = refreshStorage.get(login);
-            if (saveRefreshToken != null && saveRefreshToken.equals(refreshToken)) {
-                final UserEntity user = userService.getByLogin(login)
-                        .orElseThrow(() -> new AuthException("Пользователь не найден"));
-                final String accessToken = jwtProvider.generateAccessToken(user);
-                return new JwtResponse(accessToken, null);
-            }
-        }
-        return new JwtResponse(null, null);
-    }
-
-    @SneakyThrows
-    public JwtResponse refresh(@NonNull String refreshToken) {
-        if (jwtProvider.validateRefreshToken(refreshToken)) {
-            final Claims claims = jwtProvider.getRefreshClaims(refreshToken);
-            final String login = claims.getSubject();
-            final String saveRefreshToken = refreshStorage.get(login);
-            if (saveRefreshToken != null && saveRefreshToken.equals(refreshToken)) {
-                final UserEntity user = userService.getByLogin(login)
-                        .orElseThrow(() -> new AuthException("Пользователь не найден"));
-                final String accessToken = jwtProvider.generateAccessToken(user);
-                final String newRefreshToken = jwtProvider.generateRefreshToken(user);
-                refreshStorage.put(user.getLogin(), newRefreshToken);
-                return new JwtResponse(accessToken, newRefreshToken);
-            }
-        }
-        throw new AuthException("Невалидный JWT токен");
-    }
 
     public JwtAuthentication getAuthInfo() {
         return (JwtAuthentication) SecurityContextHolder.getContext().getAuthentication();
